e9111fa634e495604e645ec251ed11171abbe6f4
Ju5df3r <jejecamay6@gmail.com>
2025-10-18 00:02:07 +0800

    new files and folders

---
+++ src/app/components/AboutSection.jsx
@@ -0,0 +1,102 @@
+"use client";
+import React, { useTransition, useState } from "react";
+import Image from "next/image";
+import TabButton from "./TabButton";
+
+const TAB_DATA = [
+  {
+    title: "Skills",
+    id: "skills",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Node.js</li>
+        <li>React.js</li>
+        <li>Next.js</li>
+        <li>Typescript</li>
+        <li>Php</li>
+        <li>HMTL</li>
+        <li>SupaBase</li>
+        <li>MongoDB</li>
+        <li>MySql</li>
+        <li>Tailwind CSS</li>
+        <li>CSS3</li>
+        <li>Bootstrap</li>
+        <li>Laravel</li>
+        <li>Figma</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Education",
+    id: "education",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>MAJOR IN PROGRAMMING</li>
+        <li>Sibugay Technical Institute Inc.</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Certifications",
+    id: "certifications",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Best in late</li>
+        <li>Inlove niya chrr</li>
+      </ul>
+    ),
+  },
+];
+
+const AboutSection = () => {
+  const [tab, setTab] = useState("skills");
+  const [isPending, startTransition] = useTransition();
+
+  const handleTabChange = (id) => {
+    startTransition(() => {
+      setTab(id);
+    });
+  };
+
+  return (
+    <section className="text-white" id="about">
+      <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
+        <Image src="/images/YAW.jpg" width={500} height={500} />
+        <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
+          <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
+          <p className="text-base lg:text-lg">
+            Hi! I’m an aspiring Full-Stack Developer with a strong passion for Back-End Development and UI/UX Design. I love building digital experiences that are both powerful and user-friendly — combining clean, efficient code with creative and intuitive design.
+          </p>
+          <div className="flex flex-row justify-start mt-8">
+            <TabButton
+              selectTab={() => handleTabChange("skills")}
+              active={tab === "skills"}
+            >
+              {" "}
+              Skills{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("education")}
+              active={tab === "education"}
+            >
+              {" "}
+              Education{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("certifications")}
+              active={tab === "certifications"}
+            >
+              {" "}
+              Certifications{" "}
+            </TabButton>
+          </div>
+          <div className="mt-8">
+            {TAB_DATA.find((t) => t.id === tab).content}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AboutSection;
